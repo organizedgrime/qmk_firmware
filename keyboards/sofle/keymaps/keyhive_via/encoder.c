cb6047feb24aa54b51095065d61e79db480edb18
Vera Gonzalez <organizedgrime.svg@gmail.com>
2021-12-08 15:03:03 -0500

    Turned off left encoder listener because mine is broken

--- keyboards/sofle/keymaps/keyhive_via/encoder.c
+++ keyboards/sofle/keymaps/keyhive_via/encoder.c
@@ -20,14 +20,7 @@
 #ifdef ENCODER_ENABLE
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    else {
+    if (index == 1) {
         if (clockwise) {
             tap_code(KC_VOLU);
         } else {
